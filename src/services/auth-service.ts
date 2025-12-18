3c5072f19d4a7ad04e2c6f172439f60041bb8831
Iuri Guerreiro <bentoiuri@gmail.com>
2025-12-18 20:19:40 -0300

    Remove cookies de teste

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,23 +1,12 @@
-import { cookies } from "next/headers";
 import { api } from "../lib/api";
 import { UserResponse } from "../types/UserResponse";
-import { NextResponse } from "next/server";
 
 export type LoginRequest = {
   username: string;
   password: string;
 };
 
-export async function login(data: LoginRequest) {
-    const response = NextResponse.json({ ok: true });
-
-    response.cookies.set("next_test", "funcionou", {
-    httpOnly: false,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-  
+export async function login(data: LoginRequest) {  
   await api.post<void>("/auth/login", data);
 }
 
@@ -27,15 +16,5 @@ export async function logout() {
 
 export async function getMe(): Promise<UserResponse> {
   const { data } = await api.get<UserResponse>("/auth/me");
- 
-      const response = NextResponse.json({ ok: true });
-
-    response.cookies.set("next_test", "funcionou", {
-    httpOnly: false,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-
   return data;
 }
