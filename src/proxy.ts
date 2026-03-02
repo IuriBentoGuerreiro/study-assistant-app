01d07fcfacd190d1265f4a6e23c074541d092350
Iuri Guerreiro <bentoiuri@gmail.com>
2026-03-02 10:00:55 -0300

    refactor: renomeia middleware para proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,7 +8,7 @@ const PUBLIC_ROUTES = [
   "/reset-password",
 ];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (
